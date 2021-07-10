35f587235b9000c6ea876f40827aa9edc5f5fc27
mfsiat <nasirul.islam@northsouth.edu>
2021-07-10 22:01:54 +0600

    add feature: delete post

--- api/post/delete.php
+++ api/post/delete.php
@@ -4,7 +4,7 @@
   // Headers
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
-  header('Access-Control-Allow-Methods: PUT');
+  header('Access-Control-Allow-Methods: DELETE');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Header, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
   include_once '../../config/Database.php';
@@ -23,18 +23,13 @@
   // Set ID to update 
   $post->id = $data->id;
 
-  $post->title = $data->title;
-  $post->body = $data->body;
-  $post->author = $data->author;
-  $post->category_id = $data->category_id;
+  // Delete post
+  if($post->delete()) {
 
-  // Update post
-  if($post->update()) {
-
-    echo json_encode( array('message' => 'Post Updated'));
+    echo json_encode( array('message' => 'Post Deleted'));
 
   } else {
 
-    echo json_encode( array('message' => 'Post Not Updated'));
+    echo json_encode( array('message' => 'Post Not Deleted'));
     
   }
